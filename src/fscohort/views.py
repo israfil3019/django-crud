3a0d06102b4d22bbda3c2a01eff25a55bf401a52
israfil <israfil3019@gmail.com>
2021-07-10 23:17:06 +0300

    add redirect

--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,6 +1,6 @@
 from .forms import StudentForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Student
 
 
@@ -23,7 +23,13 @@ def student_list(request):
 
 def student_add(request):
     form = StudentForm()
+    if request.method == "POST":
+        print(request.POST)
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
     context = {
-        "form":form,
+        'form':form,
     }
     return render(request, "fscohort/student_add.html", context)
