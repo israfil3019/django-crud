64aada3c713dfc2fb41706d95990ea117a686478
israfil <israfil3019@gmail.com>
2021-07-10 21:39:52 +0300

    add home page with template

--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,6 +1,10 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def home(request):
-    return HttpResponse("This is home page")
+# def home(request):
+#     return HttpResponse("This is home page")
+
+
+def home_page(request):
+    return render(request, "fscohort/home.html")
