7af02f02352fea9b9c08ed84e69063123b29c44e
israfil <israfil3019@gmail.com>
2021-07-13 17:28:54 +0300

    add update page(with instance)

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home_page, student_add, student_detail, student_list
+from .views import home_page, student_add, student_detail, student_list, student_update
 
 urlpatterns = [
     path("", home_page, name="home"),
     path("list/", student_list, name="list"),
     path("add/", student_add, name="add"),
-    path("<int:id>", student_detail, name="detail"),
+    path("<int:id>/", student_detail, name="detail"),
+    path("<int:id>/update/", student_update, name="update"),
 ]
--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,6 +1,6 @@
 from .forms import StudentForm
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Student
 
 
@@ -31,7 +31,7 @@ def student_add(request):
             form.save()
             return redirect("list")
     context = {
-        'form':form,
+        'form': form,
     }
     return render(request, "fscohort/student_add.html", context)
 
@@ -42,3 +42,20 @@ def student_detail(request, id):
         "student": student
     }
     return render(request, "fscohort/student_detail.html", context)
+
+
+def student_update(request, id):
+    student = get_object_or_404(Student, id=id)
+    form = StudentForm(instance=student)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {
+        "student" : student,
+        "form" : form
+    }
+
+    return render(request, "fscohort/student_update.html", context)
